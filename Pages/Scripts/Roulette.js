b6cdbcf2a33d481f0691a436fd950f914d92480e
GILEMOS07 <161532669+GILEMOS07@users.noreply.github.com>
2024-10-28 22:00:39 -0300

    roleta

---
+++ Pages/Scripts/Roulette.js
@@ -0,0 +1,27 @@
+(function() {
+    const wheel = document.querySelector(".wheel");
+    const startButton = document.querySelector(".button");
+
+    let deg = 0;
+
+    startButton.addEventListener("click", () => {
+        const audio = new Audio('sound.mp3');
+        audio.play().catch(error => console.log("Erro ao reproduzir áudio:", error));
+
+        startButton.style.pointerEvents = "none";
+
+        deg = Math.floor(5000 + Math.random() * 5000);
+        wheel.style.transition = "transform 10s ease";
+        wheel.style.transform = `rotate(${deg}deg)`;
+        wheel.classList.add("blur");
+
+        setTimeout(() => {
+            startButton.style.pointerEvents = "auto";
+            wheel.classList.remove("blur");
+            
+            const finalDeg = deg % 360;
+            const result = Math.floor(finalDeg / (360 / 37));
+            document.getElementById("result").innerText = `Resultado: ${result}`;
+        }, 10000);
+    });
+})();
